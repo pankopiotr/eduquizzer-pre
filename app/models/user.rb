bc34f5d070a185a4e16d11b14bfccd5294541d9b
feroiso <pankopiotr1@gmail.com>
2017-08-02 22:09:07 +0200

    Validate user attributes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :email,      presence: true, length: { maximum: 64 },
+                         uniqueness: { case_sensitive: false }, email: true
+  validates :password,   presence: true, length: { in: 8..64 }
+  validates :first_name, length: { in: 2..16 }, allow_blank: true
+  validates :last_name,  length: { in: 2..32 }, allow_blank: true
+  validates :student_id, length: { in: 5..6 }, allow_blank: true,
+                         numericality: true
 end
