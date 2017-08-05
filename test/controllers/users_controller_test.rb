6e29a452c6b5a330c5814ea6923e328e24affbd6
feroiso <pankopiotr1@gmail.com>
2017-08-05 20:28:57 +0200

    Update user registration tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,23 +6,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test 'should get optional registration page' do
+  test 'should register user' do
     get register_path
     post '/register', params: { user: { email: 'john.newman@example.com',
                                         password: 'password12' } }
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
-    assert_template 'new_optional'
+    assert_not_nil User.find_by(email: 'john.newman@example.com')
   end
 
-  test 'should create user' do
+  test 'should update user with optional data' do
+    flunk('Require login implementation to work')
+    # Should fail
+    # Will redirect user to second step after login (which is not implemented yet)
     get register_path
     post '/register', params: { user: { email: 'john.newman@example.com',
                                         password: 'password12' } }
-    follow_redirect!
+    assert_select 'form label', 3
     post '/register', params: { user: { first_name: 'john', last_name: 'Newman',
                                         student_id: '123123' } }
     assert_not_nil User.find_by(student_id: '123123')
+    assert_response :redirect
   end
 end
