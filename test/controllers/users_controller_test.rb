d54a0f96bc55eef901b29f0e713a3b6f28a44724
feroiso <pankopiotr1@gmail.com>
2017-08-05 16:59:32 +0200

    Create user registration tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get registration page' do
+    get register_path
+    assert_response :success
+  end
+
+  test 'should get optional registration page' do
+    get register_path
+    post '/register', params: { user: { email: 'john.newman@example.com',
+                                        password: 'password12' } }
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+    assert_template 'new_optional'
+  end
+
+  test 'should create user' do
+    get register_path
+    post '/register', params: { user: { email: 'john.newman@example.com',
+                                        password: 'password12' } }
+    follow_redirect!
+    post '/register', params: { user: { first_name: 'john', last_name: 'Newman',
+                                        student_id: '123123' } }
+    assert_not_nil User.find_by(student_id: '123123')
+  end
 end
