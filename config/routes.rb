4b0297902b68f5b37fcfec0304db18c19ced6efd
feroiso <pankopiotr1@gmail.com>
2017-08-12 19:37:48 +0200

    Initialize internalization

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'sessions#new'
-  get '/register', to: 'users#new'
-  post '/register', to: 'users#create'
-  get '/register_optional', to: 'users#new_optional'
-  post '/register_optional', to: 'users#create_optional'
-  get '/signin', to: 'sessions#new'
-  post '/signin', to: 'sessions#create'
-  get '/signout', to: 'sessions#destroy'
+  scope '(:locale)', locale: /en/ do
+    root 'sessions#new'
+    get '/register', to: 'users#new'
+    post '/register', to: 'users#create'
+    get '/register_optional', to: 'users#new_optional'
+    post '/register_optional', to: 'users#create_optional'
+    get '/signin', to: 'sessions#new'
+    post '/signin', to: 'sessions#create'
+    get '/signout', to: 'sessions#destroy'
+  end
 end
