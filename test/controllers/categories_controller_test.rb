baa45d689ca8633cc2771cd9704fc98686d82bfa
feroiso <pankopiotr1@gmail.com>
2017-08-17 19:37:50 +0200

    Create tests for categories controller

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should get categories modify path' do
+    get categories_modify_path
+    assert :success
+  end
+
+  test 'should create category' do
+    get categories_modify_path
+    post '/categories/modify', params: { category: { name: 'Test' } }
+    assert_not_nil Category.find_by(name: 'Test')
+  end
+
+  test 'should delete category' do
+    get categories_modify_path
+    post '/categories/modify', params: { category: { name: 'Test' } }
+    assert_not_nil Category.find_by(name: 'Test')
+    delete '/categories/modify', params: { category: { name: 'Test' } }
+    assert_nil Category.find_by(name: 'Test')
+  end
 end
