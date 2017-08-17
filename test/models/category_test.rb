83d78d04d5ea754fea7d956e2245e3ff97c53d80
feroiso <pankopiotr1@gmail.com>
2017-08-17 18:06:42 +0200

    Create category model tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @category = Category.new(name: 'test')
+  end
+
+  test 'should validate category' do
+    assert @category.valid?
+  end
+
+  test 'should not validate duplicated category' do
+    @copycat_category = @category
+    @category.save
+    refute @copycat_category.valid?
+  end
+
+  test 'should not validate empty category' do
+    @category.name = ''
+    refute @category.valid?
+  end
 end
