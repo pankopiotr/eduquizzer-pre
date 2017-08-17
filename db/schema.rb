8fbd3807c19ff5c4e809a00d9852b88c3501ad8d
feroiso <pankopiotr1@gmail.com>
2017-08-17 21:01:13 +0200

    Rename type column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817154707) do
+ActiveRecord::Schema.define(version: 20170817185517) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170817154707) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
-    t.string   "type"
+    t.string   "task_type"
     t.string   "category"
     t.text     "description"
     t.text     "asset"
