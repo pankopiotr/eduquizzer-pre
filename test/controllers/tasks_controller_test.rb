a48c4067701e65f090bbbda0f92a2b7c28d03827
feroiso <pankopiotr1@gmail.com>
2017-08-19 17:31:39 +0200

    Create tests for tasks controller

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -5,4 +5,18 @@ class TasksControlerTest < ActionDispatch::IntegrationTest
     get new_task_path
     assert_response :success
   end
+
+  test 'should create task' do
+    get new_task_path
+    post '/tasks', params: { task: { name: 'Apples',
+                                     task_type: 'close-ended',
+                                     category: 'test',
+                                     description: 'Are apples blue?',
+                                     correct_solutions: %w[no nope false],
+                                     wrong_solutions: %w[yes definitely],
+                                     score: 1, mathjax: false, random: true,
+                                     no_random_solutions: 4,
+                                     min_no_random_correct_solutions: 1 } }
+    assert Task.find_by(name: 'Apples')
+  end
 end
