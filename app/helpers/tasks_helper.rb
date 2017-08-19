afd63902c7ca9de8be73bf6c2d82e3b18840a862
feroiso <pankopiotr1@gmail.com>
2017-08-19 18:22:37 +0200

    Fix wrong attribute name

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -5,7 +5,7 @@ module TasksHelper
       params.require(:task).permit(:name, :task_type, :category, :description,
                                    :asset, :score, :mathjax, :random,
                                    :no_random_solutions,
-                                   :min_no_correct_solutions,
+                                   :min_no_random_correct_solutions,
                                    correct_solutions: [], wrong_solutions: [])
     end
 end
