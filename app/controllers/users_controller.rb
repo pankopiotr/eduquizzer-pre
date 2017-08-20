7103003b186de88f0cf50030f28ddf33ec13c4ee
feroiso <pankopiotr1@gmail.com>
2017-08-20 15:51:14 +0200

    Refactor user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,4 +36,25 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  private
+
+    def user_required_params
+      params.require(:user).permit(:email, :password)
+    end
+
+    def user_optional_params
+      params.require(:user).permit(:first_name, :last_name, :student_id)
+    end
+
+    def authenticated?(remember_token)
+      return false if cookie_digest.nil?
+      BCrypt::Password.new(cookie_digest).is_password?(remember_token)
+    end
+
+    def nil_optional_attributes?
+      return if current_user.student_id.nil?
+      flash[:danger] = t(:completed_optional_once)
+      redirect_to interface_path
+    end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,25 +9,6 @@ module UsersHelper
     end
   end
 
-  def authenticated?(remember_token)
-    return false if cookie_digest.nil?
-    BCrypt::Password.new(cookie_digest).is_password?(remember_token)
-  end
-
-  def user_required_params
-    params.require(:user).permit(:email, :password)
-  end
-
-  def user_optional_params
-    params.require(:user).permit(:first_name, :last_name, :student_id)
-  end
-
-  def nil_optional_attributes?
-    return if current_user.student_id.nil?
-    flash[:danger] = t(:completed_optional_once)
-    redirect_to interface_path
-  end
-
   def generate_token
     SecureRandom.urlsafe_base64
   end
