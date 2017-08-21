1e49a953398179a1a75d364582447c58080ddf3e
feroiso <pankopiotr1@gmail.com>
2017-08-21 20:33:35 +0200

    Update authorization tests

--- test/integration/authorization_test.rb
+++ test/integration/authorization_test.rb
@@ -4,9 +4,8 @@ require 'test_helper'
 
 class AuthorizationTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:john)
-    @other_user = users(:jane)
-    @inactive_user = users(:bob)
+    @admin = users(:john)
+    @user = users(:jane)
   end
 
   test 'should not let guest user access any interface' do
@@ -14,6 +13,18 @@ class AuthorizationTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_path
     follow_redirect!
     assert_select 'div.alert-danger', count: 1
+  end
+
+  test 'should let user access user interface' do
+    sign_in_as @user
+    get interface_path
+    assert_response :success
+  end
+
+  test 'should let admin user access admin interface' do
+    sign_in_as@admin
+    get new_task_path
+    assert_response :success
   end
 
   test 'should not let normal user access admin interface' do
