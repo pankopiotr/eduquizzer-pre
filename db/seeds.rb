9b3110659c6e87d46807cffaadd8bad053cf53fe
feroiso <pankopiotr1@gmail.com>
2017-09-16 18:28:58 +0200

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+@admin = User.create(email: 'admin@example.com', password: 'admin123',
+                     first_name: 'John', last_name: 'Doe', student_id: '000000',
+                     activated: true, admin: true)
+
+(1..15).each_with_index do |_user, index|
+  User.create(email: "user#{index}@example.com", password: "user00#{index}",
+              activated: true)
+end
+
+(1..50).each_with_index do |_task, index|
+  Task.create(name: "Task no #{index}", task_type: 'Close-ended',
+              category: 'Seeds', asset: File.open('public/uploads/task/asset/2/a9537c16-7ce1-4c78-af4b-9a9d79221309.png'),
+              correct_solutions: ['First correct answer', 'Second correct answer'],
+              wrong_solutions: ['First wrong solution', 'Second wrong solution'],
+              score: index, author: @admin)
+end
