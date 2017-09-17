c58f3d0d7abbe73c85976d5f77c6e2dfab5f25bc
feroiso <pankopiotr1@gmail.com>
2017-09-17 19:22:16 +0200

    Refactor helpers

--- app/helpers/quizzes_helper.rb
+++ app/helpers/quizzes_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module QuizzesHelper
+  def author_alias(task)
+    if task.author.last_name.present? && task.author.first_name.present?
+      "#{task.author.last_name} #{task.author.first_name}."
+    else
+      task.author.email.split('@')[0]
+    end
+  end
 end
