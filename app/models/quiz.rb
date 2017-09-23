8b15985f9925fbfb01370638f56ce727678b0360
feroiso <pankopiotr1@gmail.com>
2017-09-23 19:59:12 +0200

    Set uniqueness validators to quiz name and password

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -4,8 +4,9 @@ class Quiz < ApplicationRecord
   attr_accessor :task_list
   has_and_belongs_to_many :tasks
   belongs_to :author, class_name: 'User'
-  validates :name, :password, :task_list, presence: true
-  validates :password, length: { in: 12..32 }
+  validates :name, uniqueness: { case_sensitive: false }, presence: true
+  validates :password, uniqueness: true, presence: true, length: { in: 12..32 }
+  validates :task_list, presence: true
   before_save :create_tasks_associations
 
   def add_author(user)
