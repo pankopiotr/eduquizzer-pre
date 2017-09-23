466c72cd557ef9e2861c5411d848b5356c6e5a9a
feroiso <pankopiotr1@gmail.com>
2017-09-23 13:24:19 +0200

    Complete quiz controller logic

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,11 +1,30 @@
 # frozen_string_literal: true
 
 class QuizzesController < ApplicationController
+  before_action :instantiate_task_list
+
   def new
     @quiz = Quiz.new
-    @tasks = Task.all
   end
 
   def create
+    @quiz = Quiz.new(test_params)
+    @quiz.add_author(current_user)
+    if @quiz.save
+      redirect_to new_quiz_path, flash: { success: t(:quiz_created) }
+    else
+      render 'new'
+    end
   end
+
+  private
+
+    def test_params
+      params.require(:quiz).permit(:name, :password, :random, :no_random_tasks,
+                                   :time_limit, tasks: [])
+    end
+
+    def instantiate_task_list
+      @tasks = Task.all
+    end
 end
--- test/controllers/quizzes_controller_test.rb
+++ test/controllers/quizzes_controller_test.rb
@@ -3,9 +3,24 @@
 require 'test_helper'
 
 class QuizzesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:john)
+    @tasks = [tasks(:taskApples), tasks(:taskOranges)]
+    sign_in_as@user
+  end
+
   test 'should get new' do
     get new_quiz_path
-    # assert_response :success
+    assert_response :success
+  end
+
+  test 'should create quiz' do
+    get new_quiz_path
+    post '/quizzes', params: { quiz: { name: 'TestQuiz',
+                                       password: 'Password1234',
+                                       tasks: @tasks, time_limit: 15,
+                                       random: true, no_random_tasks: 1 } }
+    assert Quiz.find_by(name: 'TestQuiz')
   end
 
 end
