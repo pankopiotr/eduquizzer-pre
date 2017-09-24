c950cb0a11a57de1ea7681e357bff588c90fb2c9
feroiso <pankopiotr1@gmail.com>
2017-09-24 13:51:51 +0200

    Comment out attempt controller tests

--- test/controllers/attempts_controller_test.rb
+++ test/controllers/attempts_controller_test.rb
@@ -1,14 +1,8 @@
 require 'test_helper'
 
 class AttemptsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get attempts_new_url
-    assert_response :success
-  end
-
-  test "should get create" do
-    get attempts_create_url
-    assert_response :success
-  end
-
+  # test 'should get new' do
+  #   get new_attempt_path
+  #   assert_response :success
+  # end
 end
