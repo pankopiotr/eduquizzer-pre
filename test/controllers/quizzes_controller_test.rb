17563ee52421bb59c5be85a05eedabd69a25908b
feroiso <pankopiotr1@gmail.com>
2017-10-12 20:25:55 +0200

    Test preventing edit on non-editable quiz

--- test/controllers/quizzes_controller_test.rb
+++ test/controllers/quizzes_controller_test.rb
@@ -6,6 +6,7 @@ class QuizzesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:john)
     sign_in_as@user
+    @quiz = quizzes(:quizFix)
   end
 
   test 'should get new' do
@@ -20,5 +21,21 @@ class QuizzesControllerTest < ActionDispatch::IntegrationTest
                                        task_list: [1, 2], time_limit: 15,
                                        random: true, no_random_tasks: 1 } }
     assert Quiz.find_by(name: 'TestQuiz')
+  end
+
+  test 'should prevent used quiz edit' do
+    get edit_quiz_path(@quiz)
+    assert_response :success
+    @quiz.mark_as_used
+    get edit_quiz_path(@quiz)
+    assert_response :redirect
+  end
+
+  test 'should prevent archived quiz edit' do
+    get edit_quiz_path(@quiz)
+    assert_response :success
+    @quiz.archive
+    get edit_quiz_path(@quiz)
+    assert_response :redirect
   end
 end
