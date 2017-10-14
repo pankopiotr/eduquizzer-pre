f7e2db1db79de0399f65d6c0e23e7ceeab2c1d9f
feroiso <pankopiotr1@gmail.com>
2017-10-14 16:40:51 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,12 @@ Category.create(name: 'Seeds')
 (1..50).each_with_index do |_task, index|
   Task.create(name: "Task no #{index}", task_type: 'Close-ended',
               category: 'Seeds', asset: File.open('public/uploads/task/asset/2/a9537c16-7ce1-4c78-af4b-9a9d79221309.png'),
-              correct_solutions: ['First correct answer', 'Second correct answer'],
-              wrong_solutions: ['First wrong solution', 'Second wrong solution'],
+              correct_solutions: ['First correct answer',
+                                  'Second correct answer',
+                                  'Third correct answer'],
+              wrong_solutions: ['First wrong solution',
+                                'Second wrong solution',
+                                'Third wrong answer'],
               score: index, author: @admin, random: true, mathjax: true,
               no_random_solutions: rand(4),
               min_no_random_correct_solutions: rand(2))
