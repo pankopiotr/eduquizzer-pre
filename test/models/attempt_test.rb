9ecd83750af1cd31f669fc1872fbe6e3e8c4e74d
feroiso <pankopiotr1@gmail.com>
2017-12-02 10:05:37 +0100

    Create tests for attempt controller

--- test/models/attempt_test.rb
+++ test/models/attempt_test.rb
@@ -1,4 +1,30 @@
+# frozen_string_literal: true)
+
 require 'test_helper'
 
 class AttemptTest < ActiveSupport::TestCase
+  def setup
+    @attempt = Attempt.new(user: users(:jane), quiz: quizzes(:quizFix),
+                           score: -9999, pieces: [pieces(:one), pieces(:two)])
+  end
+
+  test 'should validate attempt' do
+    assert @attempt.valid?
+  end
+
+  test 'should count max score' do
+    @attempt.save_score
+    assert_equal 2, @attempt.max_score
+  end
+
+  test 'should count attempt score' do
+    @attempt.save_score
+    assert_equal 1, @attempt.score
+  end
+
+  test 'should create pieces for attempt' do
+    @new_attempt = Attempt.new(user: users(:jane), quiz: quizzes(:quizFix),
+                               score: -9999).create_pieces
+    assert_equal 2, @new_attempt.pieces.count
+  end
 end
